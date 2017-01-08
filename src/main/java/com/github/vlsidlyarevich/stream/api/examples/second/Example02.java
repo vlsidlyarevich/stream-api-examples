2284163211d887147f97e2a2250dc3d1ad7cd3e2
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-08 23:36:11 +0300

    Added second example

---
+++ src/main/java/com/github/vlsidlyarevich/stream/api/examples/second/Example02.java
@@ -0,0 +1,25 @@
+package com.github.vlsidlyarevich.stream.api.examples.second;
+
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Example02 {
+
+    public static void main(String[] args) {
+        List<String> arrList = new ArrayList<>();
+        Stream<String> arrListStream = arrList.stream();  //sized, ordered
+
+        List<String> linkedList = new LinkedList<>();
+        Stream<String> linkedListStream = linkedList.stream();  //sized, ordered
+
+        Set<String> hashSet = new HashSet<>();
+        Stream<String> hashSetStream = hashSet.stream();  //sized, distinct
+
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Stream<String> linkedHashSetStream = linkedHashSet.stream();  //sized, distinct, ordered
+
+        Set<String> treeSet = new TreeSet<>();
+        Stream<String> treeSetStream = treeSet.stream();  //sized, distinct, sorted, ordered
+    }
+}
