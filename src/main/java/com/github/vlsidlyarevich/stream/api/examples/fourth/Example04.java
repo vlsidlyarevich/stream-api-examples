320352873004c1f73297f8cb6a2606547ba3598d
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-12 22:05:05 +0300

    Added Example 4

--- src/main/java/com/github/vlsidlyarevich/stream/api/examples/fourth/Example04.java
+++ src/main/java/com/github/vlsidlyarevich/stream/api/examples/fourth/Example04.java
@@ -1,21 +1,11 @@
-package com.github.vlsidlyarevich.stream.api.examples.third;
+package com.github.vlsidlyarevich.stream.api.examples.fourth;
 
-<<<<<<< Updated upstream
-import java.util.stream.Stream;
-
-=======
 
 import java.util.stream.IntStream;
->>>>>>> Stashed changes
 
-public class Example03 {
+public class Example04 {
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        Stream.iterate(1, n -> n * 2)
-                .limit(10)
-                .forEach(System.out::println);
-=======
         int sum = 0;
         for (int i = 0; i < 100; i++) {
             sum += i;
@@ -26,6 +16,5 @@ public class Example03 {
 
         System.out.println("Sum with for-loop:" + sum);
         System.out.println("Sum with int stream:" + easySum);
->>>>>>> Stashed changes
     }
 }
